7c7b0a5c78e01bd4de101e3210ca1f8fa3d225a0
tusharsaurabh <tusharacc@gmail.com>
2025-08-19 22:09:28 +0530

    Initial Commit

---
+++ polyglot-ai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  polyglot-ai
+//
+//  Created by Tushar Saurabh on 8/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ polyglot-ai/polyglot_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  polyglot_aiApp.swift
+//  polyglot-ai
+//
+//  Created by Tushar Saurabh on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct polyglot_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
